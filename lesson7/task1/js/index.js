58523aa6d401e4116e9543b360abfb67563f6e6e
Lubomyr <lubomrkashuba@gmail.com>
2021-03-25 20:36:10 +0200

    i tried my best

--- lesson7/task1/js/index.js
+++ lesson7/task1/js/index.js
@@ -1,8 +1,8 @@
 const person = {};
 person.firstName = 'Ivan';
 person.secondName = 'Ivanov';
-person.showData = function() {
-  console.log(this.firstName , this.secondName);
+person.showData = function(object) {
+  console.log(object.firstName , object.secondName);
 };
 const newPerson = {};
 for(let key in person) {
@@ -11,5 +11,5 @@ for(let key in person) {
 newPerson.firstName = 'Petro';
 newPerson.secondName = 'Petriv';
 
-person.showData();
-newPerson.showData();
+person.showData(person);
+newPerson.showData(newPerson);
